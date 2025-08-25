b6add2f852244850fa95589ab11c96fc994a2160
Blair Harris <blairehh@pm.me>
2025-08-25 16:12:57 +0200

    render variable declarations

--- src/renderer_c.rs
+++ src/renderer_c.rs
@@ -39,10 +39,23 @@ fn render_statement(statement: &CStatement) -> String {
     let mut output = match statement {
         CStatement::FunctionCall(function_call) => render_function_call(function_call),
         CStatement::Return(ret) => render_return(ret),
-        _ => panic!("TODO"),
+        CStatement::Variable(dec) => render_variable_declaration(dec),
     };
     output.push_str(";\n");
     output
+}
+
+fn render_variable_declaration(vardec: &CVariableDeclaration) -> String {
+    let mut output = String::new();
+    output.push_str(&vardec.var_type.name);
+    output.push_str(" ");
+    if vardec.is_pointer {
+        output.push_str("*");
+    }
+    output.push_str(&vardec.name);
+    output.push_str(" = ");
+    output.push_str(&render_expression(&vardec.value));
+    return output;
 }
 
 fn render_return(ret: &CReturn) -> String {
@@ -89,6 +102,7 @@ fn render_type(typ: &CType) -> &str {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use pretty_assertions::assert_eq;
 
     fn norm(input: &str) -> String {
         input
@@ -133,6 +147,137 @@ mod tests {
         int main()
         {
         printf("Hello World");
+        return 0;
+        }
+        "#;
+        let actual = render(&src);
+
+        assert_eq!(norm(&actual), norm(&expected))
+    }
+
+    #[test]
+    fn test_variable_declarations() {
+        let src = CSrc {
+            includes: vec![],
+            functions: vec![CFunction {
+                name: "main".to_string(),
+                return_type: CType {
+                    name: "int".to_string(),
+                },
+                block: CBlock {
+                    statements: vec![
+                        CVariableDeclaration {
+                            name: "a".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("1".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "b".to_string(),
+                            is_pointer: true,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("2".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "c".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("3".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "d".to_string(),
+                            is_pointer: true,
+                            var_type: CType {
+                                name: "void".to_string(),
+                            },
+                            value: CLiteral::Number("4".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "e".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("-5".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "f".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "double".to_string(),
+                            },
+                            value: CLiteral::Number("6.2".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "g".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("0".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "h".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "double".to_string(),
+                            },
+                            value: CLiteral::Number("3.14".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "my_z_var".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "long".to_string(),
+                            },
+                            value: CLiteral::Number("0".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CVariableDeclaration {
+                            name: "maybe_num".to_string(),
+                            is_pointer: false,
+                            var_type: CType {
+                                name: "int".to_string(),
+                            },
+                            value: CLiteral::Number("0".to_string()).to_expression(),
+                        }
+                        .to_statement(),
+                        CReturn {
+                            value: Some(CLiteral::Number("0".to_string()).to_expression()),
+                        }
+                        .to_statement(),
+                    ],
+                },
+            }],
+        };
+
+        let expected = r#"
+        int main()
+        {
+        int a = 1;
+        int *b = 2;
+        int c = 3;
+        void *d = 4;
+        int e = -5;
+        double f = 6.2;
+        int g = 0;
+        double h = 3.14;
+        long my_z_var = 0;
+        int maybe_num = 0;
         return 0;
         }
         "#;
